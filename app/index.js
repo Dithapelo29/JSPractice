3a961ade3e0df5b0d413da30fb235511850eb7ee
Jack Raseona <pagane.jack5@gmail.com>
2020-10-05 00:00:10 +0200

    Promises

--- app/index.js
+++ app/index.js
@@ -1,24 +1,6 @@
-let budget = ()=>{
-    let balance = 0;
+let p = new Promise((resolve,reject)=>{
+    reject('Promise rejected data');
+})
 
-    let changeBal = (val)=>{
-        return balance += val;
-    }
-
-    let deposit20 = () => changeBal(20);
-    let withdraw20 = () => changeBal(-20);
-    let check = ()=> balance;
-
-    return {
-        deposit20,check,withdraw20
-    }
-}
-
-let wallet = budget();
-wallet.deposit20();
-wallet.deposit20();
-wallet.deposit20();
-wallet.deposit20();
-wallet.withdraw20();
-
-console.log(wallet.check())
+p.then(response => console.log(response))
+.catch(err => console.log(err));
